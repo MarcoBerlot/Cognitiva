22e865136286b9826fb09aa1bc00aeb9634cca78
Marco Berlot <marco.berlot@gmail.com>
2017-11-22 02:05:07 -0500

    Changed some files for deployement

---
+++ app/install_packages_dependencies.sh
@@ -0,0 +1,11 @@
+# set the path of your project
+PATH_PROJECT=~/Development/python/projects/scheduler-i-sweated-yesterday
+
+cd ~/Downloads
+
+#
+# Installing Flask: https://github.com/mitsuhiko/flask/tags
+#
+wget https://github.com/mitsuhiko/flask/archive/0.9.zip
+unzip 0.9.zip
+mv flask-0.9/flask $PATH_PROJECT/flask
